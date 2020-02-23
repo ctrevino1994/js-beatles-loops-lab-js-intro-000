eb11913f47b3f7d023b855e2323a39713d48a7ed
Christian Trevino <ctrevino1994@yahoo.com>
2020-02-23 20:26:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ const facts = ["He was the last Beatle to learn how to drive", "He was never a v
 function johnLennonFacts(){
   while (facts < 4) {
   console.log(`${facts}!!!`)
-  facts += 1;
+  
   }
 }
