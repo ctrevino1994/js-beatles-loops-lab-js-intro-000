9c5451d884de44f5b90538c0e1fc3fd6a355d15c
Christian Trevino <ctrevino1994@yahoo.com>
2020-02-23 20:33:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,9 +11,12 @@ function theBeatlesPlay(musicians, instruments) {
 
 const facts = ["He was the last Beatle to learn how to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice" ];
 
-function johnLennonFacts(){
-  while (facts < 4) {
-  console.log(`${facts}!!!`)
-
+function johnLennonFacts(facts) {
+  var results = [];
+  var count = 0;
+  while (count < facts.length) {
+  results.push(facts[count] + "!!!");
+    count++;
   }
+  return results;
 }
